781abb4fed6e2d22acd8076012e1edf4a09e98c6
norbeyandresg <norbeyandresg@gmail.com>
2022-05-27 09:30:53 -0500

    fix next script location

--- pages/_document.js
+++ pages/_document.js
@@ -1,6 +1,5 @@
 // pages/_document.js
 import { Html, Head, Main, NextScript } from "next/document";
-
 export default function Document() {
   return (
     <Html>
@@ -15,11 +14,6 @@ export default function Document() {
           integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
           crossOrigin="anonymous"
         />
-        <script
-          src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
-          integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
-          crossOrigin="anonymous"
-        ></script>
       </Head>
       <body>
         <Main />
