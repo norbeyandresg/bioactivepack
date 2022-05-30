c2776f7938fc1f283123e96e38ead721c7b5751b
norbeyandresg <norbeyandresg@gmail.com>
2022-05-30 14:46:51 -0500

    update material card

--- components/MaterialCard.js
+++ components/MaterialCard.js
@@ -1,14 +1,74 @@
 import React from "react";
+import { Row, Col } from "reactstrap";
+
+const AccordionItem = ({ children, label, collapseId }) => (
+  <div className="accordion-item">
+    <h2 className="accordion-header" id={`${collapseId}-header`}>
+      <button
+        className="accordion-button collapsed py-2"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target={`#${collapseId}`}
+        aria-expanded="false"
+        aria-controls={collapseId}
+      >
+        {label}
+      </button>
+    </h2>
+    <div
+      id={collapseId}
+      className="accordion-collapse collapse"
+      aria-labelledby={`${collapseId}-header`}
+    >
+      <div className="accordion-body">{children}</div>
+    </div>
+  </div>
+);
 
 const MaterialCard = (m) => {
   return (
     <div className="material-card">
-      <div className="material-weight">
-        <div>{m.weight}</div>
+      <div className="card-header d-flex justify-content-between text-muted">
+        <label>weight: {m.weight}</label>
+        <label>id: {m.id}</label>
       </div>
-      <div className="card-title">{m.name}</div>
-      <div className="card-content">
-        This is a brief description of the material with some relevant elements
+      <div className="card-body d-flex flex-column">
+        <label className="fs-5 fw-bold border-bottom mb-3">{m.name}</label>
+
+        <label className="fst-italic text-muted fw-light">
+          antimicrobial agent:
+        </label>
+        <label className="card-subttitle fs-5">{m.antimicAgent}</label>
+
+        <label className="fst-italic text-muted fw-light mt-3">
+          able to inhibit:
+        </label>
+        <label className="card-subttitle ">{m.inhibit ? m.inhibit : ""}</label>
+      </div>
+      <div className="accordion card-footer card-accordion">
+        <AccordionItem label="more information" collapseId={`collapse${m.id}`}>
+          <label className="fst-italic text-muted fw-light my-3">
+            observations:
+          </label>
+          <ul className="list-group list-group-flush">
+            {m.observations
+              ? m.observations.map((o) => (
+                  <li className="list-group-item" key={o}>
+                    {o}
+                  </li>
+                ))
+              : ""}
+            <li className="list-group-item">An item</li>
+            <li className="list-group-item">A second item</li>
+            <li className="list-group-item">A third item</li>
+          </ul>
+          <label className="fst-italic text-muted fw-light mt-3">
+            references:
+          </label>
+          <label className="card-subttitle ">
+            {m.references ? m.references : ""}
+          </label>
+        </AccordionItem>
       </div>
     </div>
   );
